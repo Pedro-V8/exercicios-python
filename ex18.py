ffd76309ad1b893180b7279d5325fedf187ec178
Pedro Henrique de Deus Vieira <pedrophdv@outlook.com>
2022-05-26 14:06:20 -0300

    Adding more Hacker Rank exercises

---
+++ ex18.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+def print_int(a , b):
+    return print(int(a/b))
+def print_float(a , b):
+    return print(float(a/b))
+
+print_int(a,b)
+print_float(a,b)
---
+++ ex19.py
@@ -0,0 +1,5 @@
+n = int(input())
+
+for x in range(0,n+1):
+    if x < n:
+        print(x ** 2)
